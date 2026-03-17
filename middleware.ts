1e9f9cab3788e1d9de71263dc666420a1f3104a9
Zain-mir24 <zainnaeemk10@gmail.com>
2026-03-17 16:08:04 +0100

    added public

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-
+//
 export async function middleware(request: NextRequest) {
   let supabaseResponse = NextResponse.next({ request })
 
